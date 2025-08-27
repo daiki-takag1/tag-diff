9d5031a6c1b74e7cbedf79e6faf0d43f118a9c7c
DaikiTakagi <d-takagi@seraku.co.jp>
2025-08-27 12:12:55 +0900

    simplify

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-import { getLatestReleaseTag } from "./get_latest_release_tag.ts";
+import { getOctokit, context } from "npm:@actions/github";
+import core from "npm:@actions/core";
 
 export async function main() {
-  const latestReleaseTag = await getLatestReleaseTag();
-  console.log(latestReleaseTag);
+  const githubToken = Deno.env.get("GH_TOKEN");
+  if (!githubToken) {
+    throw new Error("GH_TOKEN is not set");
+  }
+
+  const github = getOctokit(githubToken);
+
+  const response = await github.rest.repos.getLatestRelease({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+
+  core.setOutput("latest_release_tag", response.data.tag_name);
+
+  console.log(response.data.tag_name);
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
